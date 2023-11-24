5588072c31818ed414dffb5613983b7da21e806b
Cherries-Man <2022959499@qq.com>
2023-11-24 18:30:57 +0800

    fix: 修改custom-header格式

--- simulation/src/network/utils/custom-header.h
+++ simulation/src/network/utils/custom-header.h
@@ -24,123 +24,130 @@
 #include "ns3/header.h"
 #include "ns3/int-header.h"
 
-namespace ns3 {
-/**
- * \ingroup ipv4
- *
- * \brief Custom packet header
- */
-class CustomHeader : public Header 
+namespace ns3
 {
-public:
   /**
-   * \brief Construct a null custom header
+   * \ingroup ipv4
+   *
+   * \brief Custom packet header
    */
-  CustomHeader ();
-  CustomHeader (uint32_t _headerType);
-  /**
-   * \enum EcnType
-   * \brief ECN Type defined in \RFC{3168}
-   */
-  enum EcnType
+  class CustomHeader : public Header
+  {
+  public:
+    /**
+     * \brief Construct a null custom header
+     */
+    CustomHeader();
+    CustomHeader(uint32_t _headerType);
+    /**
+     * \enum EcnType
+     * \brief ECN Type defined in \RFC{3168}
+     */
+    enum EcnType
     {
       // Prefixed with "ECN" to avoid name clash (bug 1723)
       ECN_NotECT = 0x00,
       ECN_ECT1 = 0x01,
       ECN_ECT0 = 0x02,
       ECN_CE = 0x03
-    }; 
-  /**
-   * \brief Get the type ID.
-   * \return the object TypeId
-   */
-  static TypeId GetTypeId (void);
-  virtual TypeId GetInstanceTypeId (void) const;
-  virtual void Print (std::ostream &os) const;
-  virtual uint32_t GetSerializedSize (void) const;
-  virtual void Serialize (Buffer::Iterator start) const;
-  virtual uint32_t Deserialize (Buffer::Iterator start);
+    };
+    /**
+     * \brief Get the type ID.
+     * \return the object TypeId
+     */
+    static TypeId GetTypeId(void);
+    virtual TypeId GetInstanceTypeId(void) const;
+    virtual void Print(std::ostream &os) const;
+    virtual uint32_t GetSerializedSize(void) const;
+    virtual void Serialize(Buffer::Iterator start) const;
+    virtual uint32_t Deserialize(Buffer::Iterator start);
 
-  uint32_t brief, headerType, getInt;
-  enum HeaderType{
-	L2_Header = 1,
-	L3_Header = 2,
-	L4_Header = 4
+    uint32_t brief, headerType, getInt;
+    enum HeaderType
+    {
+      L2_Header = 1,
+      L3_Header = 2,
+      L4_Header = 4
+    };
+
+    // ppp header
+    uint16_t pppProto;
+
+    // IPv4 header
+    enum FlagsE
+    {
+      DONT_FRAGMENT = (1 << 0),
+      MORE_FRAGMENTS = (1 << 1)
+    };
+    uint16_t m_payloadSize;    //!< payload size
+    uint16_t ipid;             //!< identification
+    uint32_t m_tos : 8;        //!< TOS, also used as DSCP + ECN value
+    uint32_t m_ttl : 8;        //!< TTL
+    uint32_t l3Prot : 8;       //!< Protocol
+    uint32_t ipv4Flags : 3;    //!< flags
+    uint16_t m_fragmentOffset; //!< Fragment offset
+    uint32_t sip;              //!< source address
+    uint32_t dip;              //!< destination address
+    uint16_t m_checksum;       //!< checksum
+    uint16_t m_headerSize;     //!< IP header size
+
+    union
+    {
+      struct
+      {
+        uint16_t sport;         //!< Source port
+        uint16_t dport;         //!< Destination port
+        uint32_t seq;           //!< Sequence number
+        uint32_t ack;           //!< ACK number
+        uint8_t length;         //!< Length (really a uint4_t) in words.
+        uint8_t tcpFlags;       //!< Flags (really a uint6_t)
+        uint16_t windowSize;    //!< Window size
+        uint16_t urgentPointer; //!< Urgent pointer
+        uint8_t optionBuf[32];  // buffer for storing raw options
+      } tcp;
+      struct
+      {
+        uint16_t sport; //!< Source port
+        uint16_t dport; //!< Destination port
+        uint16_t payload_size;
+        // SeqTsHeader
+        uint16_t pg;
+        uint32_t seq;
+        IntHeader ih;
+      } udp;
+      // CnHeader
+      struct
+      {
+        uint16_t fid;
+        uint8_t qIndex;
+        uint16_t qfb;
+        uint8_t ecnBits;
+        uint16_t total;
+      } cnp;
+      // qbbHeader
+      struct
+      {
+        uint16_t sport, dport;
+        uint16_t flags;
+        uint16_t pg;
+        uint32_t seq; // the qbb sequence number.
+        IntHeader ih;
+      } ack;
+      // PauseHeader
+      struct
+      {
+        uint32_t time;
+        uint32_t qlen;
+        uint8_t qIndex;
+      } pfc;
+    };
+
+    uint8_t GetIpv4EcnBits(void) const;
+    static uint32_t GetAckSerializedSize(void);
+    static uint32_t GetUdpHeaderSize(void);         // include udp, seqTs, INT
+    static uint32_t GetStaticWholeHeaderSize(void); // ppp + ip + udp + int
   };
-
-  // ppp header
-  uint16_t pppProto;
-
-  // IPv4 header
-  enum FlagsE {
-    DONT_FRAGMENT = (1<<0),
-    MORE_FRAGMENTS = (1<<1)
-  };
-  uint16_t m_payloadSize; //!< payload size
-  uint16_t ipid; //!< identification
-  uint32_t m_tos : 8; //!< TOS, also used as DSCP + ECN value
-  uint32_t m_ttl : 8; //!< TTL
-  uint32_t l3Prot: 8;  //!< Protocol
-  uint32_t ipv4Flags : 3; //!< flags
-  uint16_t m_fragmentOffset;  //!< Fragment offset
-  uint32_t sip; //!< source address
-  uint32_t dip; //!< destination address
-  uint16_t m_checksum; //!< checksum
-  uint16_t m_headerSize; //!< IP header size
-
-  union {
-	  struct {
-		  uint16_t sport;        //!< Source port
-		  uint16_t dport;   //!< Destination port
-		  uint32_t seq;  //!< Sequence number
-		  uint32_t ack;       //!< ACK number
-		  uint8_t length;             //!< Length (really a uint4_t) in words.
-		  uint8_t tcpFlags;              //!< Flags (really a uint6_t)
-		  uint16_t windowSize;        //!< Window size
-		  uint16_t urgentPointer;     //!< Urgent pointer
-		  uint8_t optionBuf[32]; // buffer for storing raw options
-	  } tcp;
-	  struct {
-		  uint16_t sport;        //!< Source port
-		  uint16_t dport;   //!< Destination port
-		  uint16_t payload_size;
-		  // SeqTsHeader
-		  uint16_t pg;
-		  uint32_t seq;
-		  IntHeader ih;
-	  } udp;
-	  // CnHeader
-	  struct {
-		  uint16_t fid;
-		  uint8_t qIndex;
-		  uint16_t qfb;
-		  uint8_t ecnBits;
-		  uint16_t total;
-	  } cnp;
-	  // qbbHeader
-	  struct {
-		  uint16_t sport, dport;
-		  uint16_t flags;
-		  uint16_t pg;
-		  uint32_t seq; // the qbb sequence number.
-		  IntHeader ih;
-	  } ack;
-	  // PauseHeader
-	  struct {
-		  uint32_t time;
-		  uint32_t qlen;
-		  uint8_t qIndex;
-	  } pfc;
-  };
-
-  uint8_t GetIpv4EcnBits (void) const;
-  static uint32_t GetAckSerializedSize(void);
-  static uint32_t GetUdpHeaderSize(void); // include udp, seqTs, INT
-  static uint32_t GetStaticWholeHeaderSize(void); // ppp + ip + udp + int
-};
 
 } // namespace ns3
 
-
 #endif /* CUSTOM_HEADER_H */
-

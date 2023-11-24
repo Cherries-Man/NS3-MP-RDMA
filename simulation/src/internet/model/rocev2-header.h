ec3d7b48d84ea9f2651583ee2915d7f2ad1e6c3b
Cherries-Man <2022959499@qq.com>
2023-11-24 19:12:09 +0800

    add: implementing rocev2-header class

--- simulation/src/internet/model/rocev2-header.h
+++ simulation/src/internet/model/rocev2-header.h
@@ -3,6 +3,7 @@
 #define ROCEV2_HEADER_H
 
 #include "ns3/header.h"
+#include "ns3/nstime.h"
 
 namespace ns3
 {
@@ -12,12 +13,32 @@ namespace ns3
     public:
         RoCEv2Header();
 
-        // Add your additional methods and member variables here
+        /**
+         * \param sync the sync signal to the receiver
+         */
+        void SetSynchronise(uint8_t sync);
+        uint8_t GetSynchronise(void) const;
+
+        /**
+         * \param reTx the retransmission signal from the receiver
+         */
+        void SetReTx(uint8_t reTx);
+        uint8_t GetReTx(void) const;
+
+        static TypeId GetTypeId(void);
+        virtual TypeId GetInstanceTypeId(void) const;
+        virtual void Print(std::ostream &os) const;
+        /**
+         * \return Returns the serialized size
+         */
+        virtual uint32_t GetSerializedSize(void) const;
 
     private:
-        // Add your member variables here
-        uint8_t synchronise;
-        uint8_t ReTx;
+        virtual void Serialize(Buffer::Iterator start) const;
+        virtual uint32_t Deserialize(Buffer::Iterator start);
+
+        uint8_t m_synchronise;
+        uint8_t m_ReTx;
     };
 
 } // namespace ns3

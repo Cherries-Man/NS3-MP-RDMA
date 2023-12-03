f3610bd532dc0f87d89d20caac5629fb64307b0b
Cherries-Man <2022959499@qq.com>
2023-12-03 17:32:32 +0800

    fix: fix code format

--- simulation/src/point-to-point/model/qbb-channel.cc
+++ simulation/src/point-to-point/model/qbb-channel.cc
@@ -25,140 +25,136 @@
 #include <iostream>
 #include <fstream>
 
-NS_LOG_COMPONENT_DEFINE ("QbbChannel");
+NS_LOG_COMPONENT_DEFINE("QbbChannel");
 
-namespace ns3 {
-
-NS_OBJECT_ENSURE_REGISTERED (QbbChannel);
-
-TypeId 
-QbbChannel::GetTypeId (void)
+namespace ns3
 {
-  static TypeId tid = TypeId ("ns3::QbbChannel")
-    .SetParent<Channel> ()
-    .AddConstructor<QbbChannel> ()
-    .AddAttribute ("Delay", "Transmission delay through the channel",
-                   TimeValue (Seconds (0)),
-                   MakeTimeAccessor (&QbbChannel::m_delay),
-                   MakeTimeChecker ())
-    .AddTraceSource ("TxRxQbb",
-                     "Trace source indicating transmission of packet from the QbbChannel, used by the Animation interface.",
-                     MakeTraceSourceAccessor (&QbbChannel::m_txrxQbb))
-  ;
-  return tid;
-}
 
-//
-// By default, you get a channel that 
-// has an "infitely" fast transmission speed and zero delay.
-QbbChannel::QbbChannel()
-  :
-    PointToPointChannel ()
-{
-  NS_LOG_FUNCTION_NOARGS ();
-  m_nDevices = 0;
-}
+  NS_OBJECT_ENSURE_REGISTERED(QbbChannel);
 
-void
-QbbChannel::Attach (Ptr<QbbNetDevice> device)
-{
-  
-  //std::cout << m_nDevices << " " << N_DEVICES << "\n";
-  //fflush(stdout);
-  NS_LOG_FUNCTION (this << device);
-  NS_ASSERT_MSG (m_nDevices < N_DEVICES, "Only two devices permitted");
-  NS_ASSERT (device != 0);
+  TypeId
+  QbbChannel::GetTypeId(void)
+  {
+    static TypeId tid = TypeId("ns3::QbbChannel")
+                            .SetParent<Channel>()
+                            .AddConstructor<QbbChannel>()
+                            .AddAttribute("Delay", "Transmission delay through the channel",
+                                          TimeValue(Seconds(0)),
+                                          MakeTimeAccessor(&QbbChannel::m_delay),
+                                          MakeTimeChecker())
+                            .AddTraceSource("TxRxQbb",
+                                            "Trace source indicating transmission of packet from the QbbChannel, used by the Animation interface.",
+                                            MakeTraceSourceAccessor(&QbbChannel::m_txrxQbb));
+    return tid;
+  }
 
-  m_link[m_nDevices++].m_src = device;
-//
-// If we have both devices connected to the channel, then finish introducing
-// the two halves and set the links to IDLE.
-//
-  if (m_nDevices == N_DEVICES)
+  //
+  // By default, you get a channel that
+  // has an "infitely" fast transmission speed and zero delay.
+  QbbChannel::QbbChannel()
+      : PointToPointChannel()
+  {
+    NS_LOG_FUNCTION_NOARGS();
+    m_nDevices = 0;
+  }
+
+  void
+  QbbChannel::Attach(Ptr<QbbNetDevice> device)
+  {
+
+    // std::cout << m_nDevices << " " << N_DEVICES << "\n";
+    // fflush(stdout);
+    NS_LOG_FUNCTION(this << device);
+    NS_ASSERT_MSG(m_nDevices < N_DEVICES, "Only two devices permitted");
+    NS_ASSERT(device != 0);
+
+    m_link[m_nDevices++].m_src = device;
+    //
+    // If we have both devices connected to the channel, then finish introducing
+    // the two halves and set the links to IDLE.
+    //
+    if (m_nDevices == N_DEVICES)
     {
       m_link[0].m_dst = m_link[1].m_src;
       m_link[1].m_dst = m_link[0].m_src;
       m_link[0].m_state = IDLE;
       m_link[1].m_state = IDLE;
     }
+  }
 
+  bool
+  QbbChannel::TransmitStart(
+      Ptr<Packet> p,
+      Ptr<QbbNetDevice> src,
+      Time txTime)
+  {
+    NS_LOG_FUNCTION(this << p << src);
+    NS_LOG_LOGIC("UID is " << p->GetUid() << ")");
 
-}
+    NS_ASSERT(m_link[0].m_state != INITIALIZING);
+    NS_ASSERT(m_link[1].m_state != INITIALIZING);
 
-bool
-QbbChannel::TransmitStart (
-  Ptr<Packet> p,
-  Ptr<QbbNetDevice> src,
-  Time txTime)
-{
-  NS_LOG_FUNCTION (this << p << src);
-  NS_LOG_LOGIC ("UID is " << p->GetUid () << ")");
+    uint32_t wire = src == m_link[0].m_src ? 0 : 1;
 
-  NS_ASSERT (m_link[0].m_state != INITIALIZING);
-  NS_ASSERT (m_link[1].m_state != INITIALIZING);
+    Simulator::ScheduleWithContext(m_link[wire].m_dst->GetNode()->GetId(),
+                                   txTime + m_delay, &QbbNetDevice::Receive,
+                                   m_link[wire].m_dst, p);
 
-  uint32_t wire = src == m_link[0].m_src ? 0 : 1;
+    // Call the tx anim callback on the net device
+    m_txrxQbb(p, src, m_link[wire].m_dst, txTime, txTime + m_delay);
+    return true;
+  }
 
-  Simulator::ScheduleWithContext (m_link[wire].m_dst->GetNode ()->GetId (),
-                                  txTime + m_delay, &QbbNetDevice::Receive,
-                                  m_link[wire].m_dst, p);
+  uint32_t
+  QbbChannel::GetNDevices(void) const
+  {
+    NS_LOG_FUNCTION_NOARGS();
 
-  // Call the tx anim callback on the net device
-  m_txrxQbb (p, src, m_link[wire].m_dst, txTime, txTime + m_delay);
-  return true;
-}
+    // std::cout<<m_nDevices<<"\n";
+    // std::cout.flush();
 
-uint32_t 
-QbbChannel::GetNDevices (void) const
-{
-  NS_LOG_FUNCTION_NOARGS ();
+    return m_nDevices;
+  }
 
-  //std::cout<<m_nDevices<<"\n";
-  //std::cout.flush();
+  Ptr<QbbNetDevice>
+  QbbChannel::GetQbbDevice(uint32_t i) const
+  {
+    NS_LOG_FUNCTION_NOARGS();
+    NS_ASSERT(i < 2);
+    return m_link[i].m_src;
+  }
 
+  Ptr<NetDevice>
+  QbbChannel::GetDevice(uint32_t i) const
+  {
+    NS_LOG_FUNCTION_NOARGS();
+    return GetQbbDevice(i);
+  }
 
-  return m_nDevices;
-}
+  Time
+  QbbChannel::GetDelay(void) const
+  {
+    return m_delay;
+  }
 
-Ptr<QbbNetDevice>
-QbbChannel::GetQbbDevice (uint32_t i) const
-{
-  NS_LOG_FUNCTION_NOARGS ();
-  NS_ASSERT (i < 2);
-  return m_link[i].m_src;
-}
+  Ptr<QbbNetDevice>
+  QbbChannel::GetSource(uint32_t i) const
+  {
+    return m_link[i].m_src;
+  }
 
-Ptr<NetDevice>
-QbbChannel::GetDevice (uint32_t i) const
-{
-  NS_LOG_FUNCTION_NOARGS ();
-  return GetQbbDevice (i);
-}
+  Ptr<QbbNetDevice>
+  QbbChannel::GetDestination(uint32_t i) const
+  {
+    return m_link[i].m_dst;
+  }
 
-Time
-QbbChannel::GetDelay (void) const
-{
-  return m_delay;
-}
-
-Ptr<QbbNetDevice>
-QbbChannel::GetSource (uint32_t i) const
-{
-  return m_link[i].m_src;
-}
-
-Ptr<QbbNetDevice>
-QbbChannel::GetDestination (uint32_t i) const
-{
-  return m_link[i].m_dst;
-}
-
-bool
-QbbChannel::IsInitialized (void) const
-{
-  NS_ASSERT (m_link[0].m_state != INITIALIZING);
-  NS_ASSERT (m_link[1].m_state != INITIALIZING);
-  return true;
-}
+  bool
+  QbbChannel::IsInitialized(void) const
+  {
+    NS_ASSERT(m_link[0].m_state != INITIALIZING);
+    NS_ASSERT(m_link[1].m_state != INITIALIZING);
+    return true;
+  }
 
 } // namespace ns3

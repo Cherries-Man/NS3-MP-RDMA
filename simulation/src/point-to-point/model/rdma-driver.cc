50cd30faac2d2a81e21744d608cac3b31ad27653
Cherries-Man <2022959499@qq.com>
2023-12-03 18:57:48 +0800

    fix: fix code format

--- simulation/src/point-to-point/model/rdma-driver.cc
+++ simulation/src/point-to-point/model/rdma-driver.cc
@@ -1,26 +1,28 @@
 #include "rdma-driver.h"
 
-namespace ns3 {
-
-/***********************
- * RdmaDriver
- **********************/
-TypeId RdmaDriver::GetTypeId (void)
+namespace ns3
 {
-	static TypeId tid = TypeId ("ns3::RdmaDriver")
-		.SetParent<Object> ()
-		.AddTraceSource ("QpComplete", "A qp completes.",
-				MakeTraceSourceAccessor (&RdmaDriver::m_traceQpComplete))
-		;
-	return tid;
-}
 
-RdmaDriver::RdmaDriver(){
-}
+	/***********************
+	 * RdmaDriver
+	 **********************/
+	TypeId RdmaDriver::GetTypeId(void)
+	{
+		static TypeId tid = TypeId("ns3::RdmaDriver")
+								.SetParent<Object>()
+								.AddTraceSource("QpComplete", "A qp completes.",
+												MakeTraceSourceAccessor(&RdmaDriver::m_traceQpComplete));
+		return tid;
+	}
 
-void RdmaDriver::Init(void){
-	Ptr<Ipv4> ipv4 = m_node->GetObject<Ipv4> ();
-	#if 0
+	RdmaDriver::RdmaDriver()
+	{
+	}
+
+	void RdmaDriver::Init(void)
+	{
+		Ptr<Ipv4> ipv4 = m_node->GetObject<Ipv4>();
+#if 0
 	m_rdma->m_nic.resize(ipv4->GetNInterfaces());
 	for (uint32_t i = 0; i < m_rdma->m_nic.size(); i++){
 		m_rdma->m_nic[i] = CreateObject<RdmaQueuePairGroup>();
@@ -29,15 +31,16 @@ void RdmaDriver::Init(void){
 			DynamicCast<QbbNetDevice>(ipv4->GetNetDevice(i))->m_rdmaEQ->m_qpGrp = m_rdma->m_nic[i];
 		}
 	}
-	#endif
-	for (uint32_t i = 0; i < m_node->GetNDevices(); i++){
-		Ptr<QbbNetDevice> dev = NULL;
-		if (m_node->GetDevice(i)->IsQbb())
-			dev = DynamicCast<QbbNetDevice>(m_node->GetDevice(i));
-		m_rdma->m_nic.push_back(RdmaInterfaceMgr(dev));
-		m_rdma->m_nic.back().qpGrp = CreateObject<RdmaQueuePairGroup>();
-	}
-	#if 0
+#endif
+		for (uint32_t i = 0; i < m_node->GetNDevices(); i++)
+		{
+			Ptr<QbbNetDevice> dev = NULL;
+			if (m_node->GetDevice(i)->IsQbb())
+				dev = DynamicCast<QbbNetDevice>(m_node->GetDevice(i));
+			m_rdma->m_nic.push_back(RdmaInterfaceMgr(dev));
+			m_rdma->m_nic.back().qpGrp = CreateObject<RdmaQueuePairGroup>();
+		}
+#if 0
 	for (uint32_t i = 0; i < ipv4->GetNInterfaces (); i++){
 		if (ipv4->GetNetDevice(i)->IsQbb() && ipv4->IsUp(i)){
 			Ptr<QbbNetDevice> dev = DynamicCast<QbbNetDevice>(ipv4->GetNetDevice(i));
@@ -46,26 +49,30 @@ void RdmaDriver::Init(void){
 			m_rdma->m_nic.back().qpGrp = CreateObject<RdmaQueuePairGroup>();
 		}
 	}
-	#endif
-	// RdmaHw do setup
-	m_rdma->SetNode(m_node);
-	m_rdma->Setup(MakeCallback(&RdmaDriver::QpComplete, this));
-}
+#endif
+		// RdmaHw do setup
+		m_rdma->SetNode(m_node);
+		m_rdma->Setup(MakeCallback(&RdmaDriver::QpComplete, this));
+	}
 
-void RdmaDriver::SetNode(Ptr<Node> node){
-	m_node = node;
-}
+	void RdmaDriver::SetNode(Ptr<Node> node)
+	{
+		m_node = node;
+	}
 
-void RdmaDriver::SetRdmaHw(Ptr<RdmaHw> rdma){
-	m_rdma = rdma;
-}
+	void RdmaDriver::SetRdmaHw(Ptr<RdmaHw> rdma)
+	{
+		m_rdma = rdma;
+	}
 
-void RdmaDriver::AddQueuePair(uint64_t size, uint16_t pg, Ipv4Address sip, Ipv4Address dip, uint16_t sport, uint16_t dport, uint32_t win, uint64_t baseRtt, Callback<void> notifyAppFinish){
-	m_rdma->AddQueuePair(size, pg, sip, dip, sport, dport, win, baseRtt, notifyAppFinish);
-}
+	void RdmaDriver::AddQueuePair(uint64_t size, uint16_t pg, Ipv4Address sip, Ipv4Address dip, uint16_t sport, uint16_t dport, uint32_t win, uint64_t baseRtt, Callback<void> notifyAppFinish)
+	{
+		m_rdma->AddQueuePair(size, pg, sip, dip, sport, dport, win, baseRtt, notifyAppFinish);
+	}
 
-void RdmaDriver::QpComplete(Ptr<RdmaQueuePair> q){
-	m_traceQpComplete(q);
-}
+	void RdmaDriver::QpComplete(Ptr<RdmaQueuePair> q)
+	{
+		m_traceQpComplete(q);
+	}
 
 } // namespace ns3

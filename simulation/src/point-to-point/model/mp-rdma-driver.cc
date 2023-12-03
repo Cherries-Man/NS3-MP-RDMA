5e531f12aa3225f6bde65443c0260ebddd841ba1
Cherries-Man <2022959499@qq.com>
2023-12-03 20:11:08 +0800

    add: add MpRdmaDriver class implementation

---
+++ simulation/src/point-to-point/model/mp-rdma-driver.cc
@@ -0,0 +1,55 @@
+#include "mp-rdma-driver.h"
+
+namespace ns3
+{
+    /*************************************
+     * MpRdmaDriver class implementation
+     ************************************/
+    TypeId MpRdmaDriver::GetTypeId(void)
+    {
+        static TypeId tid = TypeId("ns3::MpRdmaDriver")
+                                .SetParent<Object>()
+                                .AddTraceSource("QpComplete", "A qp completes.",
+                                                MakeTraceSourceAccessor(&MpRdmaDriver::m_traceQpComplete));
+        return tid;
+    }
+
+    MpRdmaDriver::MpRdmaDriver()
+    {
+    }
+
+    void MpRdmaDriver::Init(void)
+    {
+        for (uint32_t i = 0; i < m_node->GetNDevices(); i++)
+        {
+            Ptr<QbbNetDevice> dev = NULL;
+            if (m_node->GetDevice(i)->IsQbb())
+                dev = DynamicCast<QbbNetDevice>(m_node->GetDevice(i));
+            m_rdma->m_nic.push_back(MpRdmaInterfaceMgr(dev));
+            m_rdma->m_nic.back().qpGrp = CreateObject<MpRdmaQueuePairGroup>();
+        }
+        // RdmaHw do setup
+        m_rdma->SetNode(m_node);
+        m_rdma->Setup(MakeCallback(&MpRdmaDriver::QpComplete, this));
+    }
+
+    void MpRdmaDriver::SetNode(Ptr<Node> node)
+    {
+        m_node = node;
+    }
+
+    void MpRdmaDriver::SetRdmaHw(Ptr<MpRdmaHw> mpRdma)
+    {
+        m_rdma = mpRdma;
+    }
+
+    void MpRdmaDriver::AddQueuePair(uint64_t size, uint16_t pg, Ipv4Address sip, Ipv4Address dip, uint16_t sport, uint16_t dport, uint32_t win, uint64_t baseRtt, Callback<void> notifyAppFinish)
+    {
+        m_rdma->AddQueuePair(size, pg, sip, dip, sport, dport, win, baseRtt, notifyAppFinish);
+    }
+
+    void MpRdmaDriver::QpComplete(Ptr<MpRdmaQueuePair> q)
+    {
+        m_traceQpComplete(q);
+    }
+}
---
+++ simulation/src/point-to-point/model/mp-rdma-driver.h
@@ -0,0 +1,42 @@
+#ifndef MP_RDMA_DRIVER_H
+#define MP_RDMA_DRIVER_H
+
+#include <ns3/node.h>
+#include <ns3/qbb-net-device.h>
+#include <ns3/rdma.h>
+#include "mp-rdma-queue-pair.h"
+#include "mp-rdma-hw.h"
+
+namespace ns3
+{
+    class MpRdmaDriver : public Object
+    {
+    public:
+        Ptr<Node> m_node;
+        Ptr<MpRdmaHw> m_rdma;
+
+        // trace
+        TracedCallback<Ptr<MpRdmaQueuePair>> m_traceQpComplete;
+
+        static TypeId GetTypeId(void);
+        MpRdmaDriver();
+
+        // This function init the m_nic according to the NetDevice
+        // So this must be called after all NICs are installed
+        void Init(void);
+
+        // Set Node
+        void SetNode(Ptr<Node> node);
+
+        // Set RdmaHw
+        void SetRdmaHw(Ptr<MpRdmaHw> mpRdma);
+
+        // add a queue pair
+        void AddQueuePair(uint64_t size, uint16_t pg, Ipv4Address _sip, Ipv4Address _dip, uint16_t _sport, uint16_t _dport, uint32_t win, uint64_t baseRtt, Callback<void> notifyAppFinish);
+
+        // callback when qp completes
+        void QpComplete(Ptr<MpRdmaQueuePair> q);
+    };
+}
+
+#endif /* MP_RDMA_DRIVER_H */

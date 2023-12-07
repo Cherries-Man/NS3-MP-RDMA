35d8c8d24ab715eb0781657d18101b37f96b0742
Cherries-Man <2022959499@qq.com>
2023-12-07 11:28:01 +0800

    Add MpRdmaClientHelper class and its header file

---
+++ simulation/src/applications/helper/mp-rdma-client-helper.cc
@@ -0,0 +1,44 @@
+#include "mp-rdma-client-helper.h"
+#include "mp-rdma-client.h"
+#include "ns3/uinteger.h"
+#include "ns3/string.h"
+
+namespace ns3
+{
+
+    MpRdmaClientHelper::MpRdmaClientHelper()
+    {
+    }
+
+    MpRdmaClientHelper::MpRdmaClientHelper(uint16_t pg, Ipv4Address sip, Ipv4Address dip, uint16_t sport, uint16_t dport, uint64_t size, uint32_t win, uint64_t baseRtt)
+    {
+        m_factory.SetTypeId(MpRdmaClient::GetTypeId());
+        SetAttribute("PriorityGroup", UintegerValue(pg));
+        SetAttribute("SourceIP", Ipv4AddressValue(sip));
+        SetAttribute("DestIP", Ipv4AddressValue(dip));
+        SetAttribute("SourcePort", UintegerValue(sport));
+        SetAttribute("DestPort", UintegerValue(dport));
+        SetAttribute("WriteSize", UintegerValue(size));
+        SetAttribute("Window", UintegerValue(win));
+        SetAttribute("BaseRtt", UintegerValue(baseRtt));
+    }
+
+    void MpRdmaClientHelper::SetAttribute(std::string name, const AttributeValue &value)
+    {
+        m_factory.Set(name, value);
+    }
+
+    ApplicationContainer MpRdmaClientHelper::Install(NodeContainer c)
+    {
+        ApplicationContainer apps;
+        for (NodeContainer::Iterator i = c.Begin(); i != c.End(); ++i)
+        {
+            Ptr<Node> node = *i;
+            Ptr<MpRdmaClient> client = m_factory.Create<MpRdmaClient>();
+            node->AddApplication(client);
+            apps.Add(client);
+        }
+        return apps;
+    }
+
+} // namespace ns3
---
+++ simulation/src/applications/helper/mp-rdma-client-helper.h
@@ -0,0 +1,61 @@
+#ifndef MP_RDMA_CLIENT_SERVER_HELPER_H
+#define MP_RDMA_CLIENT_SERVER_HELPER_H
+
+#include <stdint.h>
+#include "ns3/application-container.h"
+#include "ns3/node-container.h"
+#include "ns3/object-factory.h"
+#include "ns3/ipv4-address.h"
+#include "mp-rdma-client.h"
+
+namespace ns3
+{
+
+    /**
+     * \brief Create a client application which does RDMA write
+     */
+    class MpRdmaClientHelper
+    {
+
+    public:
+        /**
+         * Create RdmaClientHelper which will make life easier for people trying
+         * to set up simulations with udp-client-server.
+         *
+         */
+        MpRdmaClientHelper();
+
+        /**
+         *  Create RdmaClientHelper which will make life easier for people trying
+         * to set up simulations with udp-client-server.
+         *
+         * \param ip The IP address of the remote udp server
+         * \param port The port number of the remote udp server
+         */
+
+        MpRdmaClientHelper(uint16_t pg, Ipv4Address sip, Ipv4Address dip, uint16_t sport, uint16_t dport, uint64_t size, uint32_t win, uint64_t baseRtt);
+
+        /**
+         * Record an attribute to be set in each Application after it is is created.
+         *
+         * \param name the name of the attribute to set
+         * \param value the value of the attribute to set
+         */
+        void SetAttribute(std::string name, const AttributeValue &value);
+
+        /**
+         * \param c the nodes
+         *
+         * Create one udp client application on each of the input nodes
+         *
+         * \returns the applications created, one application per input node.
+         */
+        ApplicationContainer Install(NodeContainer c);
+
+    private:
+        ObjectFactory m_factory;
+    };
+
+} // namespace ns3
+
+#endif /* MP_RDMA_CLIENT_SERVER_HELPER_H */

feedcf8b7d98b3543150df586efb50400a7d9281
Cherries-Man <2022959499@qq.com>
2023-12-15 11:22:20 +0800

    fix: add RoCEv2ACKHeader segmentation to qbb-header

--- simulation/src/point-to-point/model/qbb-header.cc
+++ simulation/src/point-to-point/model/qbb-header.cc
@@ -7,7 +7,8 @@
 
 NS_LOG_COMPONENT_DEFINE("qbbHeader");
 
-namespace ns3 {
+namespace ns3
+{
 
 	NS_OBJECT_ENSURE_REGISTERED(qbbHeader);
 
@@ -18,10 +19,12 @@ namespace ns3 {
 
 	qbbHeader::qbbHeader()
 		: m_pg(0), sport(0), dport(0), flags(0), m_seq(0)
-	{}
+	{
+	}
 
 	qbbHeader::~qbbHeader()
-	{}
+	{
+	}
 
 	void qbbHeader::SetPG(uint16_t pg)
 	{
@@ -33,22 +36,37 @@ namespace ns3 {
 		m_seq = seq;
 	}
 
-	void qbbHeader::SetSport(uint32_t _sport){
+	void qbbHeader::SetSport(uint32_t _sport)
+	{
 		sport = _sport;
 	}
-	void qbbHeader::SetDport(uint32_t _dport){
+	void qbbHeader::SetDport(uint32_t _dport)
+	{
 		dport = _dport;
 	}
 
-	void qbbHeader::SetTs(uint64_t ts){
+	void qbbHeader::SetTs(uint64_t ts)
+	{
 		NS_ASSERT_MSG(IntHeader::mode == 1, "qbbHeader cannot SetTs when IntHeader::mode != 1");
 		ih.ts = ts;
 	}
-	void qbbHeader::SetCnp(){
+	void qbbHeader::SetCnp()
+	{
 		flags |= 1 << FLAG_CNP;
 	}
-	void qbbHeader::SetIntHeader(const IntHeader &_ih){
+	void qbbHeader::SetIntHeader(const IntHeader &_ih)
+	{
 		ih = _ih;
+	}
+
+	void qbbHeader::SetReTx(uint8_t reTx)
+	{
+		ReTx = reTx;
+	}
+
+	void qbbHeader::SetAACK(uint32_t aack)
+	{
+		AACK = aack;
 	}
 
 	uint16_t qbbHeader::GetPG() const
@@ -61,48 +79,64 @@ namespace ns3 {
 		return m_seq;
 	}
 
-	uint16_t qbbHeader::GetSport() const{
+	uint16_t qbbHeader::GetSport() const
+	{
 		return sport;
 	}
-	uint16_t qbbHeader::GetDport() const{
+	uint16_t qbbHeader::GetDport() const
+	{
 		return dport;
 	}
 
-	uint64_t qbbHeader::GetTs() const {
+	uint64_t qbbHeader::GetTs() const
+	{
 		NS_ASSERT_MSG(IntHeader::mode == 1, "qbbHeader cannot GetTs when IntHeader::mode != 1");
 		return ih.ts;
 	}
-	uint8_t qbbHeader::GetCnp() const{
+
+	uint8_t qbbHeader::GetCnp() const
+	{
 		return (flags >> FLAG_CNP) & 1;
 	}
 
+	uint8_t qbbHeader::GetReTx() const
+	{
+		return ReTx;
+	}
+
+	uint32_t qbbHeader::GetAACK() const
+	{
+		return AACK;
+	}
+
 	TypeId
-		qbbHeader::GetTypeId(void)
+	qbbHeader::GetTypeId(void)
 	{
 		static TypeId tid = TypeId("ns3::qbbHeader")
-			.SetParent<Header>()
-			.AddConstructor<qbbHeader>()
-			;
+								.SetParent<Header>()
+								.AddConstructor<qbbHeader>();
 		return tid;
 	}
 	TypeId
-		qbbHeader::GetInstanceTypeId(void) const
+	qbbHeader::GetInstanceTypeId(void) const
 	{
 		return GetTypeId();
 	}
 	void qbbHeader::Print(std::ostream &os) const
 	{
-		os << "qbb:" << "pg=" << m_pg << ",seq=" << m_seq;
+		os << "qbb:"
+		   << "pg=" << m_pg << ",seq=" << m_seq;
 	}
-	uint32_t qbbHeader::GetSerializedSize(void)  const
+	uint32_t qbbHeader::GetSerializedSize(void) const
 	{
 		return GetBaseSize() + IntHeader::GetStaticSize();
 	}
-	uint32_t qbbHeader::GetBaseSize() {
+	uint32_t qbbHeader::GetBaseSize()
+	{
 		qbbHeader tmp;
-		return sizeof(tmp.sport) + sizeof(tmp.dport) + sizeof(tmp.flags) + sizeof(tmp.m_pg) + sizeof(tmp.m_seq);
+		return sizeof(tmp.sport) + sizeof(tmp.dport) + sizeof(tmp.flags) + sizeof(tmp.m_pg) + sizeof(tmp.m_seq) + sizeof(ReTx) + sizeof(AACK);
 	}
-	void qbbHeader::Serialize(Buffer::Iterator start)  const
+	void qbbHeader::Serialize(Buffer::Iterator start) const
 	{
 		Buffer::Iterator i = start;
 		i.WriteU16(sport);
@@ -113,6 +147,8 @@ namespace ns3 {
 
 		// write IntHeader
 		ih.Serialize(i);
+		i.WriteU8(ReTx);
+		i.WriteU32(AACK);
 	}
 
 	uint32_t qbbHeader::Deserialize(Buffer::Iterator start)
@@ -126,6 +162,8 @@ namespace ns3 {
 
 		// read IntHeader
 		ih.Deserialize(i);
+		ReTx = i.ReadU8();
+		AACK = i.ReadU32();
 		return GetSerializedSize();
 	}
 }; // namespace ns3

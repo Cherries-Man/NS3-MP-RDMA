55104d576cae9bd3a99fd74445c09dde581e1ea5
Cherries-Man <2022959499@qq.com>
2023-12-25 16:25:21 +0800

    add: add test file

---
+++ simulation/build/ns3/myf-hello.h
@@ -0,0 +1,5 @@
+class MyfHello
+{
+public:
+    void hello();
+};
---
+++ simulation/src/point-to-point/model/myf-hello.cc
@@ -0,0 +1,7 @@
+#include <ns3/myf-hello.h>
+#include <iostream>
+
+void MyfHello::hello()
+{
+    std::cout << "Hello World, NS3" << std::endl;
+}
